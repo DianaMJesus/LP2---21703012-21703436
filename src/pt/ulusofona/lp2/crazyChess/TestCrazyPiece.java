23dfdcc1a2ed000c7faaf88363a653b432972df4
Diana Jesus <a21703012@alunos.ulht.pt>
2018-11-29 17:01:32 +0000

    Primeiro push

--- src/pt/ulusofona/lp2/crazyChess/TestCrazyPiece.java
+++ src/pt/ulusofona/lp2/crazyChess/TestCrazyPiece.java
@@ -1,4 +1,13 @@
 package pt.ulusofona.lp2.crazyChess;
 
+import org.junit.Test;
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class TestCrazyPiece {
+    //@Test
+
 }
--- src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
+++ src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
@@ -1,8 +1,24 @@
 package pt.ulusofona.lp2.crazyChess;
 
 import org.junit.Test;
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestSimulador {
-    //@Test
+    @Test
+    public void faltaFicheiro(){
+        Simulador simulador = new Simulador();
+        boolean resultado = simulador.iniciaJogo(new File(""));
+        assertFalse("Não selecionou nenhum ficheiro",resultado);
+    }
 
+    @Test
+    public void tentaJogada(){
+        Simulador simulador = new Simulador();
+        boolean resultado = simulador.processaJogada(2,3,5,8);
+        assertFalse("Não foi possivel passar no teste",resultado);
+    }
 }
