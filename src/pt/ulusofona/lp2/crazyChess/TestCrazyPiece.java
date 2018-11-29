961a050a9b005998ac69c9e525528636205690e3
Diana Jesus <a21703012@alunos.ulht.pt>
2018-11-29 17:21:03 +0000

    Primeiro push

--- src/pt/ulusofona/lp2/crazyChess/TestCrazyPiece.java
+++ src/pt/ulusofona/lp2/crazyChess/TestCrazyPiece.java
@@ -1,13 +1,24 @@
 package pt.ulusofona.lp2.crazyChess;
 
 import org.junit.Test;
-import java.io.File;
-import java.util.List;
+import static junit.framework.TestCase.assertEquals;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class TestCrazyPiece {
-    //@Test
+    @Test
+    public void testeEquipa(){
+        CrazyPiece peace = new CrazyPiece(1,0,1,"João Ratão");
+        int equipaEsperada = 1;
+        int equipaObtida = peace.getEquipa();
+        assertEquals(equipaEsperada,equipaObtida);
+    }
+
+    @Test
+    public void testeTipoPeca(){
+        CrazyPiece peace = new CrazyPiece(7,0,0,"Joaquim Couves");
+        int tipoEsperado = 0;
+        int tipoObtido = peace.getEquipa();
+        assertEquals(tipoEsperado,tipoObtido);
+    }
 
 }
--- src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
+++ src/pt/ulusofona/lp2/crazyChess/TestSimulador.java
@@ -2,10 +2,7 @@ package pt.ulusofona.lp2.crazyChess;
 
 import org.junit.Test;
 import java.io.File;
-import java.util.List;
-
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
 
 public class TestSimulador {
     @Test
